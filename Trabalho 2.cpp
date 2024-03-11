499be3a2444656da9b546a042477329024d9bcf3
willian <128076027+willlg@users.noreply.github.com>
2024-03-10 21:22:48 -0300

    Update Trabalho 2.cpp
    mostrar iteracoes e x0

--- Trabalho 2.cpp
+++ Trabalho 2.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-vector<double> jacobi(vector<vector<double>>& matrizA, vector<double>& vetorB, int maxIteracoes, double tolerancia) {
+vector<double> jacobi(const vector<vector<double>>& matrizA, const vector<double>& vetorB, const vector<double>& x0, int maxIteracoes, double tolerancia, int& iteracoes) {
     int n = matrizA.size();
-    vector<double> x(n, 0.0);
+    vector<double> x(x0);
     vector<double> xNovo(n, 0.0);
 
-    for (int iteracao = 0; iteracao < maxIteracoes; ++iteracao) {
+    for (iteracoes = 0; iteracoes < maxIteracoes; ++iteracoes) {
         for (int i = 0; i < n; ++i) {
             double soma = 0.0;
             for (int j = 0; j < n; ++j) {
@@ -35,11 +35,11 @@ vector<double> jacobi(vector<vector<double>>& matrizA, vector<double>& vetorB, i
     return x;
 }
 
-vector<double> gaussSeidel(vector<vector<double>>& matrizA, vector<double>& vetorB, int maxIteracoes, double tolerancia) {
+vector<double> gaussSeidel(const vector<vector<double>>& matrizA, const vector<double>& vetorB, const vector<double>& x0, int maxIteracoes, double tolerancia, int& iteracoes) {
     int n = matrizA.size();
-    vector<double> x(n, 0.0);
+    vector<double> x(x0);
 
-    for (int iteracao = 0; iteracao < maxIteracoes; ++iteracao) {
+    for (iteracoes = 0; iteracoes < maxIteracoes; ++iteracoes) {
         bool convergiu = true;
         for (int i = 0; i < n; ++i) {
             double soma = 0.0;
@@ -92,12 +92,14 @@ vector<double> gaussEliminacao(vector<vector<double>>& matrizA, vector<double>& 
 
 int main() {
     int tamanho;
-    cout << "Insira o tamanho da matriz (ex: 4 para uma matriz 4x4): ";
+    cout << "Insira o tamanho da matriz (ex: 3 para uma matriz 3x3): ";
     cin >> tamanho;
 
     vector<vector<double>> matrizA(tamanho, vector<double>(tamanho, 0.0));
     vector<double> vetorB(tamanho, 0.0);
     vector<double> x(tamanho, 0.0);
+    vector<double> x0 = {0, 0, 0}; // Vetor inicial
+
     int maxIteracoes;
     double tolerancia;
 
@@ -119,6 +121,7 @@ int main() {
     cout << "Insira o valor de tolerancia: ";
     cin >> tolerancia;
 
+    int iteracoes;
     cout << "Escolha o método para resolver o sistema de equações:\n";
     cout << "1. Jacobi\n";
     cout << "2. Gauss-Seidel\n";
@@ -129,10 +132,10 @@ int main() {
 
     switch (opcao) {
         case 1:
-            x = jacobi(matrizA, vetorB, maxIteracoes, tolerancia);
+            x = jacobi(matrizA, vetorB, x0, maxIteracoes, tolerancia, iteracoes);
             break;
         case 2:
-            x = gaussSeidel(matrizA, vetorB, maxIteracoes, tolerancia);
+            x = gaussSeidel(matrizA, vetorB, x0, maxIteracoes, tolerancia, iteracoes);
             break;
         case 3:
             x = gaussEliminacao(matrizA, vetorB);
@@ -147,5 +150,9 @@ int main() {
         cout << "x[" << i << "] = " << x[i] << "\n";
     }
 
+    cout << "Número de iterações: " << iteracoes << endl;
+
     return 0;
 }
+
+
